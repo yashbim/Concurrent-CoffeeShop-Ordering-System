d9fbf2f083676b35b2eb8c50a366f71b88a3fc82
yashbim <ybimsara03@gmail.com>
2025-01-17 00:35:32 +0530

    Added created class details for coffeeshop class

--- src/main/java/org/example/CoffeeShop.java
+++ src/main/java/org/example/CoffeeShop.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CoffeeShop {
+
+    private Queue<String> coffeeOrders;
+    private int maxOrders;
+
+    public CoffeeShop(int maxOrders) {
+        this.coffeeOrders = new LinkedList<>();
+        this.maxOrders = maxOrders;
+    }
 }
