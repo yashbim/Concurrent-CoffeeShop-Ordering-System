6c70b8698cf1fa937851025003a74470102cbef7
yashbim <ybimsara03@gmail.com>
2025-01-17 00:39:45 +0530

    Created function for placing order

--- src/main/java/org/example/CoffeeShop.java
+++ src/main/java/org/example/CoffeeShop.java
@@ -12,4 +12,17 @@ public class CoffeeShop {
         this.coffeeOrders = new LinkedList<>();
         this.maxOrders = maxOrders;
     }
+
+    public synchronized void addOrder(String order) {
+        while (coffeeOrders.size() >= maxOrders) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        coffeeOrders.add(order);
+        notifyAll();
+        System.out.println("Order added: " + order + "\nRemaining orders: " + coffeeOrders.size() + "\nMaximal orders: " + maxOrders);
+    }
 }
