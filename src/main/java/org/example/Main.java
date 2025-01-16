efe5260003a8de2753425e579365bc9c4876d2f1
yashbim <ybimsara03@gmail.com>
2025-01-17 01:24:56 +0530

    Threads for customer created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
+// Source code available at https://github.com/yashbim/Concurrent-CoffeeShop-Ordering-System
+
 package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+//        System.out.println("Hello, World!");
+
+        CoffeeShop coffeeShop = new CoffeeShop(5);
+        // customer threads
+        Thread[] customerThreads = new Thread[10];
+        for (int i = 0; i < customerThreads.length; i++) {
+            customerThreads[i] = new Thread(new Customer(coffeeShop, "Customer : " + i));
+            try {
+                Thread.sleep(1000); //customer arrival time difference
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e.getMessage());
+            }
+            customerThreads[i].start();
+        }
     }
 }
