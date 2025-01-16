30f12430f0d0e9341f64527cfd57ac6cc64ff862
yashbim <ybimsara03@gmail.com>
2025-01-17 01:11:07 +0530

    Run method for barista added

--- src/main/java/org/example/Barista.java
+++ src/main/java/org/example/Barista.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Barista {
+public class Barista  implements Runnable{
 
     private CoffeeShop coffeeShop;
     private String baristaName;
@@ -9,5 +9,26 @@ public class Barista {
         this.coffeeShop = coffeeShop;
         this.baristaName = baristaName;
     }
-    
+
+    @Override
+    public void run() {
+        while (true) {
+            String order = coffeeShop.makeOrder();
+            if (order == null) {
+                break;
+            }
+
+            try{
+                Thread.sleep(1000); // preperation time
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e.getMessage());
+                break;
+            }
+            System.out.println(order + " was prepared by " + baristaName);
+            System.out.println("Remaining orders: " + coffeeShop.getRemainingOrders());
+        }
+
+    }
+
 }
