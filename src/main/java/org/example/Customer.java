738907635a25c33831fc771fe89426a37ebda1b0
yashbim <ybimsara03@gmail.com>
2025-01-17 00:54:28 +0530

    Implemented customer class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,9 +1,23 @@
 package org.example;
 
 public class Customer implements Runnable {
+    private CoffeeShop coffeeShop;
+    private String customerName;
+
+    public Customer(CoffeeShop coffeeShop, String customerName) {
+        this.coffeeShop = coffeeShop;
+        this.customerName = customerName;
+    }
 
     @Override
     public void run() {
-
+        String order = customerName + " wants a coffee. ";
+        coffeeShop.addOrder(order);
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println(e.getMessage());
+        }
     }
 }
