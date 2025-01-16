900f53886bb26b24f8d673dcd20d2e450320fa7c
yashbim <ybimsara03@gmail.com>
2025-01-17 01:07:37 +0530

    Added properties for barista in barista.java

--- src/main/java/org/example/Barista.java
+++ src/main/java/org/example/Barista.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Barista {
+
+    private CoffeeShop coffeeShop;
+    private String baristaName;
+
+    public Barista(CoffeeShop coffeeShop, String baristaName) {
+        this.coffeeShop = coffeeShop;
+        this.baristaName = baristaName;
+    }
+    
 }
