ec6952ba2a9655228c2437542a12293ddbcd8c50
yashbim <ybimsara03@gmail.com>
2025-01-17 01:43:06 +0530

    Remaining thread functions added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
         CoffeeShop coffeeShop = new CoffeeShop(5);
         // customer threads
         Thread[] customerThreads = new Thread[10];
-        for (int i = 0; i < customerThreads.length; i++) {
+        for (int i = 0; i < 10; i++) {
             customerThreads[i] = new Thread(new Customer(coffeeShop, "Customer : " + i));
             try {
                 Thread.sleep(1000); //customer arrival time difference
@@ -21,10 +21,39 @@ public class Main {
             customerThreads[i].start();
         }
 
+        //barista threads
         Thread[] baristaThreads = new Thread[2];
-        for (int i = 0; i < baristaThreads.length; i++) {
+        for (int i = 0; i < 2; i++) {
             baristaThreads[i] = new Thread(new Barista(coffeeShop, "Barista : " + i));
             baristaThreads[i].start();
         }
+
+        //wait for customer threads
+        for (Thread customerThread : customerThreads) {
+            try {
+                customerThread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e.getMessage());
+            }
+        }
+
+        //wait for barista
+        for (Thread baristaThread : baristaThreads) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println(e.getMessage());
+            }
+        }
+
+        //stop barista threads to end
+
+        for (Thread baristaThread : baristaThreads) {
+            baristaThread.interrupt();
+        }
+
+        System.out.println("Orders completed");
     }
 }
